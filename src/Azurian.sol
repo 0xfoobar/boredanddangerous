b50b91f79a3759283e072fc95c16bfd9886584c3
0xfoobar <0xfoobar1@gmail.com>
2023-07-07 20:54:43 -0400

    azurian and tests

---
+++ src/Azurian.sol
@@ -0,0 +1,154 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.20;
+
+import {MultiOwnable} from "./MultiOwnable.sol";
+
+import {ERC721} from "solmate/tokens/ERC721.sol";
+import {ERC2981} from "openzeppelin-contracts/contracts/token/common/ERC2981.sol";
+import {Strings} from "openzeppelin-contracts/contracts/utils/Strings.sol";
+import {DefaultOperatorFilterer} from "operator-filter-registry/DefaultOperatorFilterer.sol";
+
+import {IAzurian} from "./AzurRoot.sol";
+
+contract Azurian is IAzurian, DefaultOperatorFilterer, ERC721, ERC2981, MultiOwnable {
+    /// @notice The bored and dangerous contract
+    address public immutable ROOT;
+
+    /// @notice Total number of tokens which have minted
+    uint256 public totalSupply = 0;
+
+    /// @notice The prefix to attach to the tokenId to get the metadata uri
+    string public baseTokenURI;
+
+    /// @notice Whether the minting is open
+    bool public mintOpen;
+
+    /// @notice Thrown when the mint is not yet open
+    error MintNotOpen();
+
+    /// @notice Thrown when an ether transfer fails
+    error FailedToSendEther();
+
+    constructor(address _root) ERC721("Azurian", "AZUR") {
+        ROOT = _root;
+    }
+
+    function burnRootAndMint(uint256[] calldata rootIds) external {
+        if(msg.sender != ROOT) revert AccessControl();
+        if(!mintOpen) revert MintNotOpen();
+        unchecked {
+            for (uint256 i = 0; i < rootIds.length; ++i) {
+                _mint(tx.origin, rootIds[i]);
+            }
+        }
+        totalSupply += rootIds.length;
+    }
+
+    /////////////////////////
+    // ADMIN FUNCTIONALITY //
+    /////////////////////////
+
+    /// @notice Set metadata
+    function setBaseTokenURI(string memory _baseTokenURI) external {
+        if (msg.sender != metadataOwner) {
+            revert AccessControl();
+        }
+        baseTokenURI = _baseTokenURI;
+    }
+
+    /// @notice Set mint open
+    function setMintOpen(bool _mintOpen) external {
+        if (msg.sender != mintingOwner) {
+            revert AccessControl();
+        }
+        mintOpen = _mintOpen;
+    }
+
+    /// @notice Claim funds
+    function claimFunds(address payable recipient) external {
+        if (!(msg.sender == mintingOwner || msg.sender == metadataOwner || msg.sender == royaltyOwner)) {
+            revert AccessControl();
+        }
+
+        (bool sent,) = recipient.call{value: address(this).balance}("");
+        if (!sent) {
+            revert FailedToSendEther();
+        }
+    }
+
+    // ROYALTY FUNCTIONALITY
+
+    /// @dev See {IERC165-supportsInterface}.
+    function supportsInterface(bytes4 interfaceId) public pure override(ERC721, ERC2981) returns (bool) {
+        return interfaceId == 0x2a55205a // ERC165 Interface ID for ERC2981
+            || interfaceId == 0x01ffc9a7 // ERC165 Interface ID for ERC165
+            || interfaceId == 0x80ac58cd // ERC165 Interface ID for ERC721
+            || interfaceId == 0x5b5e139f; // ERC165 Interface ID for ERC721Metadata
+    }
+
+    /// @dev See {ERC2981-_setDefaultRoyalty}.
+    function setDefaultRoyalty(address receiver, uint96 feeNumerator) external {
+        if (msg.sender != royaltyOwner) {
+            revert AccessControl();
+        }
+        _setDefaultRoyalty(receiver, feeNumerator);
+    }
+
+    /// @dev See {ERC2981-_deleteDefaultRoyalty}.
+    function deleteDefaultRoyalty() external {
+        if (msg.sender != royaltyOwner) {
+            revert AccessControl();
+        }
+        _deleteDefaultRoyalty();
+    }
+
+    /// @dev See {ERC2981-_setTokenRoyalty}.
+    function setTokenRoyalty(uint256 tokenId, address receiver, uint96 feeNumerator) external {
+        if (msg.sender != royaltyOwner) {
+            revert AccessControl();
+        }
+        _setTokenRoyalty(tokenId, receiver, feeNumerator);
+    }
+
+    /// @dev See {ERC2981-_resetTokenRoyalty}.
+    function resetTokenRoyalty(uint256 tokenId) external {
+        if (msg.sender != royaltyOwner) {
+            revert AccessControl();
+        }
+        _resetTokenRoyalty(tokenId);
+    }
+
+    // METADATA FUNCTIONALITY
+
+    /// @notice Returns the metadata URI for a given token
+    function tokenURI(uint256 tokenId) public view override returns (string memory) {
+        return string(abi.encodePacked(baseTokenURI, Strings.toString(tokenId)));
+    }
+
+    // OPERATOR FILTER
+
+    function setApprovalForAll(address operator, bool approved) public override onlyAllowedOperatorApproval(operator) {
+        super.setApprovalForAll(operator, approved);
+    }
+
+    function approve(address operator, uint256 tokenId) public override onlyAllowedOperatorApproval(operator) {
+        super.approve(operator, tokenId);
+    }
+
+    function transferFrom(address from, address to, uint256 tokenId) public override onlyAllowedOperator(from) {
+        super.transferFrom(from, to, tokenId);
+    }
+
+    function safeTransferFrom(address from, address to, uint256 tokenId) public override onlyAllowedOperator(from) {
+        super.safeTransferFrom(from, to, tokenId);
+    }
+
+    function safeTransferFrom(address from, address to, uint256 tokenId, bytes calldata data)
+        public
+        override
+        onlyAllowedOperator(from)
+    {
+        super.safeTransferFrom(from, to, tokenId, data);
+    }
+}
---
+++ src/test/Azurian.t.sol
@@ -0,0 +1,121 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "ds-test/test.sol";
+import "forge-std/console2.sol";
+import "forge-std/Test.sol";
+
+import {AzurRoot} from "../AzurRoot.sol";
+import {Azurian} from "../Azurian.sol";
+import {IDelegationRegistry} from "../IDelegationRegistry.sol";
+
+interface Book {
+    function ownerOf(uint256 tokenId) external view returns (address);
+    function transferFrom(address from, address to, uint256 id) external;
+    function setApprovalForAll(address spender, bool approved) external;
+    function claimFunds(address payable recipient) external;
+}
+
+contract AzurianTest is Test {
+    IDelegationRegistry registry = IDelegationRegistry(0x00000000000076A84feF008CDAbe6409d2FE638B);
+    Azurian azurian = new Azurian(0x0025C3ABfa72E7c509ad458b50982835404A1d6c);
+    AzurRoot root = AzurRoot(azurian.ROOT());
+    Book book = Book(root.BOOK());
+    address user = 0xFC48426Da0338735945BaDEf273736cCFF53A358;
+    address delegate = 0x28C6c06298d514Db089934071355E5743bf21d60;
+
+    receive() external payable {}
+
+    function setUp() public {
+        vm.deal(user, 1000 ether);
+
+        vm.label(address(this), "Tester");
+        vm.label(address(book), "Book");
+        vm.label(address(root), "Root");
+        vm.label(address(azurian), "Azurian");
+    }
+
+    function testBurnRoots() public {
+        // This requires a fork of mainnet
+        if (block.timestamp <= 1000) {
+            return;
+        }
+
+        vm.startPrank(user);
+        uint256[] memory tokenIds = new uint[](2);
+        tokenIds[0] = 6863;
+        tokenIds[1] = 6739;
+
+        // Fail before mint opens
+        vm.expectRevert(abi.encodeWithSelector(Azurian.MintNotOpen.selector));
+        root.burnRoots(address(azurian), tokenIds);
+        vm.stopPrank();
+
+        azurian.setMintOpen(true);
+
+        // Prank msg.sender and tx.origin for this one
+        vm.startPrank(user, user);
+        root.burnRoots(address(azurian), tokenIds);
+        vm.stopPrank();
+
+        // Test that azurian minted to user
+        assertEq(azurian.ownerOf(tokenIds[0]), user);
+
+        // Test that roots are burned
+        vm.expectRevert();
+        root.ownerOf(tokenIds[0]);
+    }
+
+    function testBurnRootsDelegate() public {
+        // This requires a fork of mainnet
+        if (block.timestamp <= 1000) {
+            return;
+        }
+
+        vm.startPrank(user);
+        registry.delegateForAll(delegate, true);
+        vm.stopPrank();
+
+        vm.startPrank(delegate);
+        uint256[] memory tokenIds = new uint[](2);
+        tokenIds[0] = 6863;
+        tokenIds[1] = 6739;
+
+        // Fail before mint opens
+        vm.expectRevert(abi.encodeWithSelector(Azurian.MintNotOpen.selector));
+        root.burnRoots(address(azurian), tokenIds);
+        vm.stopPrank();
+
+        azurian.setMintOpen(true);
+
+        // Fail if unrelated third party tries to call
+        address hacker = 0xDB8723FdC67E515565040C2F6e43c1b8ba0d6a6b;
+        vm.startPrank(hacker, hacker);
+        vm.expectRevert();
+        root.burnRoots(address(azurian), tokenIds);
+        vm.stopPrank();
+
+        // Prank msg.sender and tx.origin for this one
+        vm.startPrank(delegate, delegate);
+        root.burnRoots(address(azurian), tokenIds);
+        vm.stopPrank();
+
+        // Test that azurian minted to user
+        assertEq(azurian.ownerOf(tokenIds[0]), delegate);
+
+        // Test that roots are burned
+        vm.expectRevert();
+        root.ownerOf(tokenIds[0]);
+    }
+
+    function testClaimFunds() public {
+        // Deal funds into the contract
+        uint256 value = 1 ether;
+        vm.deal(address(azurian), value);
+
+        // Then claim them
+        uint256 prevBalance = address(this).balance;
+        azurian.claimFunds(payable(address(this)));
+        assertEq(address(this).balance - prevBalance, value);
+    }
+}
